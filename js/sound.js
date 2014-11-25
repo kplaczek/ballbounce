4291c58f1542269462b2aa09b928d56f1bc00da4
kplaczek <kplaczek@wi.ps.pl>
2014-11-25 16:43:32 +0100

    update sounds a bit

--- js/sound.js
+++ js/sound.js
@@ -1,14 +1,37 @@
 function Sound() {
     this.colission = 'http://www.freesound.org/data/previews/22/22768_132693-lq.mp3';
     this.shoot = 'http://www.freesound.org/data/previews/98/98190_1354367-lq.mp3';
+    this.volume = 1;
+    document.getElementById('volume').addEventListener('change', this.setVolume, true);
+    this.sfx = 1;
 }
 
+Sound.prototype.setVolume = function (volume) {
+    if (typeof volume === 'object') {
+        volume = volume.target.value;
+    }
+    volume = parseInt(volume) / 100;
+    if (volume >= 0 && 1 >= volume) {
+        game.sound.volume = volume;
+    }
+};
+
+Sound.prototype.getVolume = function () {
+    return this.volume;
+};
+
 Sound.prototype.colissionPlay = function () {
-    var snd = new Audio(this.colission); 
-    snd.play();
+    if (this.sfx) {
+        var snd = new Audio(this.colission);
+        snd.volume = this.getVolume();
+        snd.play();
+    }
 };
 
 Sound.prototype.shootPlay = function () {
-    var snd = new Audio(this.shoot); 
-    snd.play();
+    if (this.sfx) {
+        var snd = new Audio(this.shoot);
+        snd.volume = this.getVolume();
+        snd.play();
+    }
 };
