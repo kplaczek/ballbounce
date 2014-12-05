4d4577c3785a41d828f1435e74412757d8ce9015
kplaczek <kplaczek@wi.ps.pl>
2014-12-05 19:15:43 +0100

    powerups begining

---
+++ js/powerup.js
@@ -0,0 +1,29 @@
+function Powerup() {
+
+    var inventory = [];
+
+
+}
+
+Powerup.prototype.newPowerup = function (type) {
+    switch (type) {
+        case 'ammo':
+
+            break;
+        case 'barier':
+
+            break;
+        case 'bonusPoints':
+
+            break;
+        case 'money':
+
+            break;
+        case 'repair':
+
+            break;
+        case 'speedUp':
+
+            break;
+    }
+};
